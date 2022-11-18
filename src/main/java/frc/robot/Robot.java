39c9e4d00080c81a6e1cd3b41f2f97955c0e2af3
unknown <nmarks413@gmail.com>
2022-11-17 17:16:45 -0800

    add auto :D

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.commands.AutoDrive;
 import frc.robot.commands.Drive;
 
 public class Robot extends TimedRobot {
@@ -15,27 +17,38 @@ public class Robot extends TimedRobot {
 
   private Drivetrain drivetrain = new Drivetrain(1, 2);
   private Drive drive;
+  private Command autoCommand = new SequentialCommandGroup(
+    new AutoDrive(drivetrain, 4, 4).withTimeout(2),
+    new AutoDrive(drivetrain, 0, 3).withTimeout(1)
+  );
 
   @Override
   public void robotInit() {
     this.lj = new Joystick(0);
     this.rj = new Joystick(1);
-    drive = new Drive(this.drivetrain, this.lj, this.rj);
+    this.drive = new Drive(this.drivetrain, this.lj, this.rj);
   }
 
   @Override
   public void teleopInit() {
-    drive.schedule();
-    drive.initialize();
+    this.autoCommand.end(false);
+    this.drive.schedule();
+    this.drive.initialize();
   }
 
   @Override
   public void teleopPeriodic() {
-    drive.execute();
+    this.drive.execute();
   }
 
   @Override
   public void autonomousInit() {
-    drive.end(true);
+    this.drive.end(true);
+    this.autoCommand.schedule();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    this.autoCommand.execute();
   }
 }
